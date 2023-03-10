4ff44677c54696b0eff0e2cd6971d4b821531285
Luca Lucareli <luca.lucareli@germinare.org.br>
2023-03-10 16:17:45 -0300

    teste

--- Cliente.java
+++ Cliente.java
@@ -2,10 +2,9 @@ public class Cliente {
     private String tipoConta;
     private String nome;
     private String cpf;
-<<<<<<< HEAD
     private int agenciaLuca;
-=======
+
     private int agenciaHeitor;
->>>>>>> cc08cbc (Adicionando uma alteração numa variavel)
+
     private double saldo;
 }
